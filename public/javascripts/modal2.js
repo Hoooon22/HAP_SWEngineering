9d8d93017928cd3614225c1eec3c650e6d4dbf42
강성희 <kkxxh@kangseonghee-MacBookPro.local>
2020-12-01 01:52:18 +0900

    modify_modal change

--- public/javascripts/modal2.js
+++ public/javascripts/modal2.js
@@ -2,13 +2,20 @@
 var modal2 = document.getElementsByClassName("modal2")[0];
 var modal_button2 = document.getElementById("modal_button2");
 var close_button = document.getElementById("close_button");
+var btn_submit = document.getElementById("btn_submit");
 
 modal_button2.addEventListener("click",function(e){
     modal2.style.display = "flex";
 });
+
 
 close_button.addEventListener("click",function(e){
    
     modal2.style.display="none";    
 
 }); 
+
+btn_submit.addEventListener("click",function(e){
+
+    modal2.style.display="none";
+});
