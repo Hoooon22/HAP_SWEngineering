28deee6581681827c1620aa60020e5c66d7586a2
강성희 <kkxxh@kangseonghee-MacBookPro.local>
2020-11-30 21:32:22 +0900

    change date format

--- public/javascripts/questionBoard.js
+++ public/javascripts/questionBoard.js
@@ -26,47 +26,6 @@ for (let i in qboard_count) {
 rowCount; //게시판의 총 개수
 pageCount; //게시판의 페이지 개수
 
-function btn_submit_click() {
-    const NOW_DATE = new Date()  
-    var nowDay = "";
-    switch (NOW_DATE.getDay()) {
-        case 0:
-            nowDay = "SUN";
-            break;
-        case 1:
-            nowDay = "MON";
-            break;
-        case 2:
-            nowDay = "TUE";
-            break;
-        case 3:
-            nowDay = "WED";
-            break;
-        case 4:
-            nowDay = "THU";
-            break;
-        case 5:
-            nowDay = "FRI";
-            break;
-        case 6:
-            nowDay = "SAT";
-            break;
-        default:
-            nowDay = " ";
-            break;
-    }
-    //document.write(NOW_DATE.getFullYear()+NOW_MONTH);
-    const NOW_MONTH = NOW_DATE.getMonth() + 1;
-    //document.write(NOW_MONTH);
-    //document.write(NOW_DATE.getDate());
-    //document.write(nowDay);
-    //document.write(NOW_DATE.getHours());
-    //document.write(NOW_DATE.getMinutes());
-
-
-    var printDate = NOW_DATE.getFullYear()+NOW_MONTH+NOW_DATE.getDate()+nowday+NOW_DATE.getHours()+NOW_DATE.getMinutes();
-    document.getElementById("date_value").value=printDate;
-}
 
 
 
