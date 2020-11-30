5d0cc3a75c68868f4051fc5507d09e4d5404d75e
강성희 <kkxxh@kangseonghee-MacBookPro.local>
2020-11-30 20:15:33 +0900

    date

--- public/javascripts/questionBoard.js
+++ public/javascripts/questionBoard.js
@@ -9,23 +9,64 @@ var qboard_reply = document.getElementById("reply_value").innerText;
 qboard_reply = qboard_reply.split(',');
 // count 
 var qboard_count = document.getElementById("count_value").innerText;
-var rowCount=10;
-var pageCount=0;
+var rowCount = 10;
+var pageCount = 0;
+var printDate;
 
-for (let i in qboard_count)
-{
+for (let i in qboard_count) {
     qboard[i] = {
         title: qboard_title[i],
         content: qboard_content[i],
         u_id: qboard_u_id[i],
         reply: qboard_reply[i],
     }
-    if(rowCount%15) pageCount++;
+    if (rowCount % 15) pageCount++;
 }
 
 rowCount; //게시판의 총 개수
 pageCount; //게시판의 페이지 개수
 
+function btn_submit_click() {
+    const NOW_DATE = new Date()  
+    var nowDay = "";
+    switch (NOW_DATE.getDay()) {
+        case 0:
+            nowDay = "SUN";
+            break;
+        case 1:
+            nowDay = "MON";
+            break;
+        case 2:
+            nowDay = "TUE";
+            break;
+        case 3:
+            nowDay = "WED";
+            break;
+        case 4:
+            nowDay = "THU";
+            break;
+        case 5:
+            nowDay = "FRI";
+            break;
+        case 6:
+            nowDay = "SAT";
+            break;
+        default:
+            nowDay = " ";
+            break;
+    }
+    //document.write(NOW_DATE.getFullYear()+NOW_MONTH);
+    const NOW_MONTH = NOW_DATE.getMonth() + 1;
+    //document.write(NOW_MONTH);
+    //document.write(NOW_DATE.getDate());
+    //document.write(nowDay);
+    //document.write(NOW_DATE.getHours());
+    //document.write(NOW_DATE.getMinutes());
+
+
+    var printDate = NOW_DATE.getFullYear()+NOW_MONTH+NOW_DATE.getDate()+nowday+NOW_DATE.getHours()+NOW_DATE.getMinutes();
+    document.getElementById("date_value").value=printDate;
+}
 
 
 
